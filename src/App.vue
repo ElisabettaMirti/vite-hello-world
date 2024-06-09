8384f8bf7fd7102ba2fdc44bbb3031efa4710c83
ElisabettaMirti <belmirti@gmail.com>
2024-06-09 19:04:56 +0200

    importato componente AppTitle in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
-<script setup>
+<script>
+import AppTitle from './components/AppTitle.vue';
 
+export default {
+  components: {
+    AppTitle
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
 </script>
 
 <template>
 
+<header>
+  <AppTitle />
+</header>
 
 </template>
 
